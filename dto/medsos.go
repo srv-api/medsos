5f88cefd0fd37e88ceb590f2890fddfac30e917e
srv-kirim <inbox.kirim@gmail.com>
2026-04-27 12:31:59 +0700

    form input

--- dto/medsos.go
+++ dto/medsos.go
@@ -3,13 +3,13 @@ package dto
 import "mime/multipart"
 
 type MedsosRequest struct {
-	ID        string                `json:"id"`
-	UserID    string                `json:"user_id"`
-	Caption   string                `json:"caption"`
-	DetailID  string                `json:"detail_id"`
-	CreatedBy string                `json:"created_by"`
-	ImageURL  string                `form:"image_url"`
-	Image     *multipart.FileHeader `form:"image"`
+	ID        string                `form:"id" json:"id"`
+	UserID    string                `form:"user_id"json:"user_id"`
+	Caption   string                `form:"caption"json:"caption"`
+	DetailID  string                `form:"detail_id"json:"detail_id"`
+	CreatedBy string                `form:"created_by"json:"created_by"`
+	ImageURL  string                `form:"image_url"form:"image_url"`
+	Image     *multipart.FileHeader `form:"image"form:"image"`
 }
 
 type MedsosResponse struct {
