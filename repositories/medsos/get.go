6dbcad47f658b77c7893f711b5471720c9fab96d
srv-kirim <inbox.kirim@gmail.com>
2026-04-27 16:52:48 +0700

    form input

--- repositories/medsos/get.go
+++ repositories/medsos/get.go
@@ -8,24 +8,31 @@ func (r *medsosRepository) Get(req dto.MatchFeedRequest) ([]dto.MedsosResponse, 
 	var medsosResponses []dto.MedsosResponse
 
 	query := `
+        SELECT id, caption, detail_id, image_url, user_id, created_by
+        FROM medsos
+        WHERE user_id = ?
+        
+        UNION ALL
+        
         SELECT m.id, m.caption, m.detail_id, m.image_url, m.user_id, m.created_by
         FROM medsos m
         WHERE m.user_id IN (
             SELECT 
                 CASE 
-                    WHEN user1_id = ? THEN user2_id
-                    WHEN user2_id = ? THEN user1_id
+                    WHEN matches.user1_id = ? THEN matches.user2_id
+                    WHEN matches.user2_id = ? THEN matches.user1_id
                 END
             FROM matches
-            WHERE (user1_id = ? OR user2_id = ?)
+            WHERE (matches.user1_id = ? OR matches.user2_id = ?)
         )
-        AND m.user_id != ?
+        
+        ORDER BY id DESC
     `
 
 	err := r.DB.Raw(query,
-		req.UserID, req.UserID, // untuk CASE
-		req.UserID, req.UserID, // untuk WHERE clause
-		req.UserID, // untuk AND m.user_id != ?
+		req.UserID,
+		req.UserID, req.UserID,
+		req.UserID, req.UserID,
 	).Scan(&medsosResponses).Error
 
 	if err != nil {
